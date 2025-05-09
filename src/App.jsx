78f0a68bf989ee6425992d5ff2186d6b1d678e77
kresteni <umantodchristine@gmail.com>
2025-05-09 07:59:25 +0700

    updd

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,7 @@ function AppContent() {
       />
       <main className="container mx-auto px-4 py-8">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/role-selection" element={<RoleSelection />} />
