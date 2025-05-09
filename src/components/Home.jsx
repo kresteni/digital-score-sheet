9ff402d8be97f5fc0485b9152a75f8d7423ce2f0
kresteni <umantodchristine@gmail.com>
2025-05-09 07:37:23 +0700

    up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Header from "./Header";
 import GameSetup from "./GameSetup";
 import GameScreen from "./GameScreen";
 import GameSummary from "./GameSummary";
